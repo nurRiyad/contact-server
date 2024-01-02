8c4f43337d0b6c3801a7cfb274e9dc34077b3286
nurRiyad <asadnurriyad@gmail.com>
2024-01-02 22:49:43 +0600

    Update contract data
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- routes/Contract.route.js
+++ routes/Contract.route.js
@@ -3,10 +3,14 @@ const express = require('express');
 const route = express.Router();
 
 route.post('/', (req, res, next) => {
-  res.send('This is contract id api');
+  res.send('Send all contract data');
 });
 
 route.get('/:contractId', (req, res, next) => {
+  res.send('send a single contract');
+});
+
+route.post('/:contractId', (req, res, next) => {
   res.send('This is contract id api');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const contract = require('./Contract.route');
-const { getAllContracts } = require('../controllers/contracts');
 
 const router = express.Router();
 
-router.get('/contracts', getAllContracts);
 router.use('/contract', contract);
 
 module.exports = router;
