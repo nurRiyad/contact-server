8f95d1ca11f18b0e26011c11ffd2b2fc0ad73fd2
nurRiyad <asadnurriyad@gmail.com>
2024-01-08 22:58:42 +0600

    Add basic user data
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,42 @@
 const express = require('express')
+const User = require('../model/user')
 
 const route = express.Router()
 
-route.get('/', (req, res) => {
-  res.send('Send a user data')
+route.get('/', async (req, res, next) => {
+  try {
+    const { email } = req.params
+    const user = await User.findOne({ email }, { number: 1, name: 1, _id: 0 })
+    if (user) res.send(user)
+    else next({ status: 404, message: 'No resource found' })
+  } catch (error) {
+    next(error)
+  }
 })
 
-route.patch('/', (req, res) => {
-  res.send('Update a user data')
+route.patch('/', async (req, res, next) => {
+  try {
+    const { name, password, email } = req.body
+    if (!name && !password) next({ status: 400, message: 'Bad request' })
+    else {
+      const user = await User.findOneAndUpdate({ email }, { name, password }, { returnDocument: 'after' })
+      if (user) res.send(user)
+      else next({ status: 404, message: 'User not found' })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-route.delete('/', (req, res) => {
-  res.send('Delete A user')
+route.delete('/', async (req, res, next) => {
+  try {
+    const { userId } = req.params
+    const contract = await User.findOneAndDelete({ id: userId })
+    if (contract) res.send(contract)
+    else next({ status: 404, message: 'Contract not found' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = route
