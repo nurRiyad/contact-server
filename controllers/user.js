d1008a09a99060ea903c0b4b67e4a4b40530e54c
nurRiyad <asadnurriyad@gmail.com>
2024-02-26 23:45:41 +0600

    User api fix
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,11 @@
 const User = require('../model/user')
 const joi = require('joi')
+const hashAString = require('../utils/hash')
 
 const getUser = async (req, res, next) => {
   try {
-    const { email } = req.params
-    const user = await User.findOne({ email }, { number: 1, name: 1, _id: 0 })
+    const { email } = req.user
+    const user = await User.findOne({ email }, { password: 0, __v: 0 })
     if (user) res.send(user)
     else next({ status: 404, message: 'No resource found' })
   } catch (error) {
@@ -15,16 +16,20 @@ const getUser = async (req, res, next) => {
 const updateUser = async (req, res, next) => {
   try {
     const schema = joi.object({
-      email: joi.string().email().required(),
-      password: joi.string().min(4),
-      name: joi.string().min(3).alphanum()
+      username: joi.string().min(3).alphanum(),
+      password: joi.string().min(4)
     })
 
-    const { name, password, email } = req.body
-    const { error } = schema.validate({ email, password, name })
+    const { username, password } = req.body
+    const { error } = schema.validate({ username, password })
     if (error) next({ status: 400, message: error.message || 'Bad request' })
     else {
-      const user = await User.findOneAndUpdate({ email }, { name, password }, { returnDocument: 'after' })
+      const { email: loginUserEmail } = req.user
+      const user = await User.findOneAndUpdate(
+        { email: loginUserEmail },
+        { username, password: hashAString(password) },
+        { returnDocument: 'after' }
+      )
       if (user) res.send(user)
       else next({ status: 404, message: 'User not found' })
     }
