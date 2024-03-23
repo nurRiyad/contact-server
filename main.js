4433f86ac70d99cb5e713074ac080f832b6e48eb
nurRiyad <asadnurriyad@gmail.com>
2024-03-23 13:30:44 +0600

    Update cors
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
@@ -15,6 +16,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({ credentials: true, origin: true }))
+
 app.use(morgan('tiny'))
 app.use(helmet())
 
